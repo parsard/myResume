421c147a420754c14c9a0669a5d14a98bc4cad33
parsard <parsaroodhastam@gmail.com>
2024-12-04 15:09:38 +0330

    basic js added

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-// function number(apple,orange){
-//     const juice=`juice made by ${apple} and ${orange}`
-//     return juice
-// }
+'use strict'
 
-// console.log(number(3,4))
+const btnAbout = document.querySelector('.btn-about');
+const btnWork = document.querySelector('.btn-work');
+const btnContact = document.querySelector('.btn-contact');
+const about = document.querySelector('.about-container')
+const work = document.querySelector('.work-container')
+const contact = document.querySelector('.contact-container')
+const icons= document.querySelectorAll('.right-nav-link')
+console.log(icons)
 
+btnAbout.addEventListener('click', function () {
+   
+
+    about.scrollIntoView({ behavior:'smooth'})
+})
+
+btnWork.addEventListener('click', function () {
+    work.scrollIntoView({behavior:'smooth'})
+})
+
+btnContact.addEventListener('click', function () {
+    contact.scrollIntoView({behavior:'smooth'})
+})
+
+for (let i = 0; i < icons.length; i++){
+    icons[i].addEventListener('click', function () {
+        contact.scrollIntoView({behavior:'smooth'})
+    })
+}
